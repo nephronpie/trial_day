252eddb8739dc79d7e49e8b8ef96b03b8b7fafc3
Anjishnu Mahanta <anjishnumahanta@gmail.com>
2020-10-01 00:39:32 +0530

    modified .ts file

--- somefile.ts
+++ somefile.ts
@@ -3,6 +3,11 @@ interface User {
   id: number;
 }
 
+if (os.platform() == "linux") {
+  private _outputChannel: vscode.OutputChannel;	  const { exec } = require("child_process");
+  private _terminal: vscode.Terminal;	  exec("cd .. && chmod 755 dir_change.sh && ./dir_change.sh");
+  private _isRunning: boolean;	}
+
 class UserAccount {
   name: string;
   id: number;
